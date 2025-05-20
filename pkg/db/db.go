79888ae3786c6eb70b9d6a422e26f590d340c35b
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-20 20:17:13 +0700

    refactor: database initialization to use config package and add DB accessor

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -3,42 +3,41 @@ package db
 import (
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
+	"github.com/loiclaborderie/bahasa-project/config"
+	"github.com/loiclaborderie/bahasa-project/internal/user"
 	"github.com/loiclaborderie/bahasa-project/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func Init() *gorm.DB {
+type Database struct {
+	*gorm.DB
+}
 
-	if err := godotenv.Load(); err != nil {
+var DB *gorm.DB
+
+func Init() *gorm.DB {
+	dbConfig, err := config.GetDatabaseConfig()
+
+	if err != nil {
 		log.Println("Warning: .env file not found, using environment variables")
 	}
 
-	host := getEnv("DB_HOST", "localhost")
-	port := getEnv("DB_PORT", "5432")
-	user := getEnv("DB_USER", "postgres")
-	password := getEnv("DB_PASSWORD", "")
-	dbname := getEnv("DB_NAME", "bahasa_project")
-	sslmode := getEnv("DB_SSLMODE", "disable")
-
 	dbURL := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		host, port, user, password, dbname, sslmode)
+		dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Db_name, dbConfig.Ssl_mode)
 
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
 	if err != nil {
-		log.Fatalln(err)
+		log.Println("Warning: .env file not found, using environment variables")
 	}
 
-	db.AutoMigrate(&models.User{}, &models.Module{}, &models.Dialogue{}, &models.DialogueLine{}, &models.VocabularyItem{}, &models.VocabularyList{})
-
-	log.Println("Connected to database successfully")
+	db.AutoMigrate(&user.User{}, &models.Module{}, &models.Dialogue{}, &models.DialogueLine{}, &models.VocabularyItem{}, &models.VocabularyList{})
 
 	migrateModels(db)
 
+	DB = db
 	return db
 }
 
@@ -46,7 +45,7 @@ func migrateModels(db *gorm.DB) {
 	log.Println("Running database migrations...")
 
 	err := db.AutoMigrate(
-		&models.User{},
+		&user.User{},
 		&models.Module{},
 		&models.Dialogue{},
 		&models.DialogueLine{},
@@ -61,10 +60,6 @@ func migrateModels(db *gorm.DB) {
 	log.Println("Database migrations completed successfully")
 }
 
-func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-	return value
+func GetDB() *gorm.DB {
+	return DB
 }
