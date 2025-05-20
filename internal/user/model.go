437867ae88bd21f4f6cc759e4df5f6aa81621371
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-20 21:10:52 +0700

    Add Role field to User model with validation constraints

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,15 +1,17 @@
 package user
 
 import (
+	"github.com/loiclaborderie/bahasa-project/constants"
 	"gorm.io/gorm"
 )
 
 // User represents a user in the system
 type User struct {
 	gorm.Model
-	Username  string `json:"username" binding:"required"`
-	Email     string `json:"email" binding:"required,email"`
-	Password  string `json:"-"` // Password is not returned in JSON
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	Username  string         `json:"username" binding:"required"`
+	Email     string         `json:"email" binding:"required,email"`
+	Password  string         `json:"-"` // Password is not returned in JSON
+	FirstName string         `json:"first_name"`
+	LastName  string         `json:"last_name"`
+	Role      constants.Role `json:"-" binding:"required,eq=ADMIN|eq=USER"`
 }
