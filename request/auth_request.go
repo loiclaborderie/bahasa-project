1643ec887b5e3dd563dd1f0b171a1a63c4d05326
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-22 14:36:52 +0700

    chore: add authentication request structs for registration and login

---
+++ request/auth_request.go
@@ -0,0 +1,12 @@
+package request
+
+type RegisterRequest struct {
+	Username string `validate:"required" json:"username"`
+	Email    string `validate:"required" json:"email"`
+	Password string `validate:"required" json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `validate:"required" json:"email"`
+	Password string `validate:"required" json:"password"`
+}
