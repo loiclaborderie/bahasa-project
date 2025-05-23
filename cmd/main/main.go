17910f29cde9b278df83c957071b7ebf7614f42c
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-23 17:45:32 +0700

    chore: add module and dialogue endpoints with dependency injection

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,12 +3,39 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
+	"github.com/loiclaborderie/bahasa-project/internal/dialogue"
+	"github.com/loiclaborderie/bahasa-project/internal/module"
+	"github.com/loiclaborderie/bahasa-project/internal/user"
+	"github.com/loiclaborderie/bahasa-project/pkg/db"
 	"github.com/loiclaborderie/bahasa-project/routes"
 )
 
 func main() {
-	router := routes.SetupRouter()
+	database := db.Init()
 
+	userRepo := user.NewUserRepository(database)
+	userService := user.NewUserService(userRepo)
+	userHandler := user.NewUserHandler(userService)
+
+	moduleRepo := module.NewModuleRepository(database)
+	moduleService := module.NewModuleService(moduleRepo)
+	moduleHandler := module.NewModuleHandler(moduleService)
+
+	dialogueRepo := dialogue.NewRepository(database)
+	dialogueService := dialogue.NewService(dialogueRepo)
+	dialogueHandler := dialogue.NewHandler(dialogueService)
+
+	router := gin.Default()
+
+	router.GET("/admin/modules", moduleHandler.GetAllModules)
+	router.GET("/modules", moduleHandler.GetVisibleModules)
+	router.POST("/modules", moduleHandler.Create)
+	// router.GET("/modules/:id", moduleHandler.FindByID)
+	router.GET("/modules/:id", moduleHandler.FindByID)
+	router.GET("/dialogue/:id/speakers", dialogueHandler.GetAvailableSpeaker)
+	router.GET("/dialogue/:id", dialogueHandler.GetDialogue)
+	routes.RegisterAuthRoutes(database, router)
 	log.Println("Server starting on :8080")
 	err := router.Run(":8080")
 	if err != nil {
