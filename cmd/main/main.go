abf3c493d2c48127ec4cd7f166634d7856492b6d
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-28 10:38:31 +0700

    chore: Add CORS middleware with configurable allowed origins using environment variable

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/loiclaborderie/bahasa-project/config"
 	"github.com/loiclaborderie/bahasa-project/internal/dialogue"
 	"github.com/loiclaborderie/bahasa-project/internal/module"
-	"github.com/loiclaborderie/bahasa-project/internal/user"
 	"github.com/loiclaborderie/bahasa-project/pkg/db"
 	"github.com/loiclaborderie/bahasa-project/routes"
 )
@@ -14,9 +16,9 @@ import (
 func main() {
 	database := db.Init()
 
-	userRepo := user.NewUserRepository(database)
-	userService := user.NewUserService(userRepo)
-	userHandler := user.NewUserHandler(userService)
+	// userRepo := user.NewUserRepository(database)
+	// userService := user.NewUserService(userRepo)
+	// userHandler := user.NewUserHandler(userService)
 
 	moduleRepo := module.NewModuleRepository(database)
 	moduleService := module.NewModuleService(moduleRepo)
@@ -27,6 +29,15 @@ func main() {
 	dialogueHandler := dialogue.NewHandler(dialogueService)
 
 	router := gin.Default()
+	// corsConfig.AllowOrigins = []string{config.GetEnv("ALLOW_ORIGIN", "")}
+	fmt.Println("config :", config.GetEnv("ALLOW_ORIGIN", ""))
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{config.GetEnv("ALLOW_ORIGIN", "")}, // or set to your dev frontend
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	router.GET("/admin/modules", moduleHandler.GetAllModules)
 	router.GET("/modules", moduleHandler.GetVisibleModules)
