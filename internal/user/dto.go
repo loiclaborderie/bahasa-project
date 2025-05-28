f9a32fba64191812ce97aac2685ec761eeb85d18
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-28 10:43:19 +0700

    chore: add UserResponse DTO and conversion method for user data serialization

---
+++ internal/user/dto.go
@@ -0,0 +1,19 @@
+package user
+
+type UserResponse struct {
+	ID        uint   `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func (u User) ToUserResponse() UserResponse {
+	return UserResponse{
+		ID:        u.ID,
+		Username:  u.Username,
+		Email:     u.Email,
+		FirstName: u.FirstName,
+		LastName:  u.LastName,
+	}
+}
