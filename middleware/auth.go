7e495c64647083c91879adcb7a076eabe1cb2cd6
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-28 10:41:20 +0700

    refactor: moved util func ValidateTokenAndGetUser to token to reuse it later, made a validate token helper func. Switched to cookie based jwt auth

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,63 +1,16 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/loiclaborderie/bahasa-project/constants"
-	"github.com/loiclaborderie/bahasa-project/internal/user"
-	"github.com/loiclaborderie/bahasa-project/pkg/db"
+	"github.com/loiclaborderie/bahasa-project/pkg/helper"
 )
-
-// Helper function to handle the token validation and user retrieval
-func validateTokenAndGetUser(c *gin.Context) (*user.User, error) {
-	authHeader := c.GetHeader("Authorization")
-	if authHeader == "" {
-		return nil, fmt.Errorf("you don't have the rights")
-	}
-
-	authToken := strings.Split(authHeader, " ")
-	if len(authToken) != 2 || authToken[0] != "Bearer" {
-		return nil, fmt.Errorf("invalid token format")
-	}
-
-	tokenStr := authToken[1]
-	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(os.Getenv("JWT_SECRET")), nil
-	})
-
-	if err != nil || !token.Valid {
-		return nil, fmt.Errorf("invalid or expired token")
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		return nil, fmt.Errorf("invalid token")
-	}
-
-	if float64(time.Now().Unix()) > claims["exp"].(float64) {
-		return nil, fmt.Errorf("token expired")
-	}
-
-	var user user.User
-	if err := db.GetDB().Where("email = ?", claims["email"]).Find(&user).Error; err != nil || user.ID == 0 {
-		return nil, fmt.Errorf("user not found")
-	}
-
-	return &user, nil
-}
 
 func RequireAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		_, err := validateTokenAndGetUser(c)
+		_, err := helper.ValidateTokenAndGetUser(c)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 			c.AbortWithStatus(http.StatusUnauthorized)
@@ -70,7 +23,7 @@ func RequireAuth() gin.HandlerFunc {
 
 func RequiresRole(roleRequired constants.Role) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		user, err := validateTokenAndGetUser(c)
+		user, err := helper.ValidateTokenAndGetUser(c)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 			c.AbortWithStatus(http.StatusUnauthorized)
--- pkg/helper/token.go
+++ pkg/helper/token.go
@@ -1,10 +1,14 @@
 package helper
 
 import (
+	"fmt"
 	"os"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/loiclaborderie/bahasa-project/internal/user"
+	"github.com/loiclaborderie/bahasa-project/pkg/db"
 )
 
 func CreateToken(email string) (string, error) {
@@ -21,4 +25,49 @@ func CreateToken(email string) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func ValidateToken(tokenStr string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, fmt.Errorf("invalid or expired token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	if float64(time.Now().Unix()) > claims["exp"].(float64) {
+		return nil, fmt.Errorf("token expired")
+	}
+
+	return claims, nil
+}
+
+func ValidateTokenAndGetUser(c *gin.Context) (*user.User, error) {
+	tokenString, cookieErr := c.Cookie("token")
+
+	if cookieErr != nil {
+		return nil, fmt.Errorf("no auth token was found")
+	}
+
+	claims, err := ValidateToken(tokenString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var user user.User
+	if err := db.GetDB().Where("email = ?", claims["email"]).Find(&user).Error; err != nil || user.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return &user, nil
 }
