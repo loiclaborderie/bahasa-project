3fef4c6f42b6618a63d080041a24a5b74615d1ab
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-05-28 10:42:40 +0700

    chore: add logout and me endpoints with authentication middleware to auth routes

--- routes/auth.go
+++ routes/auth.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/loiclaborderie/bahasa-project/internal/auth"
+	"github.com/loiclaborderie/bahasa-project/middleware"
 	"gorm.io/gorm"
 )
 
@@ -11,4 +12,6 @@ func RegisterAuthRoutes(db *gorm.DB, router *gin.Engine) {
 	auth := auth.NewAuthHandlerImpl(db, validator.New())
 	router.POST("/register", auth.Register)
 	router.POST("/login", auth.Login)
+	router.POST("/logout", middleware.RequireAuth(), auth.Logout)
+	router.GET("/me", middleware.RequireAuth(), auth.Me)
 }
